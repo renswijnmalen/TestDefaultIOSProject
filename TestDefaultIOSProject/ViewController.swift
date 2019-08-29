c8f858bde800713a828de0bf6e84efb97eb0a6b0
Rens Wijnmalen <r.wijnmalen@yipyip.nl>
2019-08-29 07:13:19 +0200

    Default ios project

---
+++ TestDefaultIOSProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestDefaultIOSProject
+//
+//  Created by Rens Wijnmalen on 29/08/2019.
+//  Copyright © 2019 RensWijnmalen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
